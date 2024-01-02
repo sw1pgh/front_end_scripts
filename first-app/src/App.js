19d37d0151f892a1c0d0e91495933fd04acaa00b
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-01-02 21:35:48 +0530

    ubuntu-create-react-app

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first reactjs App
+          Welcome to my first reactjs App.
         </p>
         <a
           className="App-link"
