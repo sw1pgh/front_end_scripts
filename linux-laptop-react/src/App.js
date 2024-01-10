48efb7e6bb00e49a9e2c80f19760734018d93830
Swapnaneel Ghosh <swapnaneel.ghosh@geotechinfo.net>
2024-01-10 09:41:22 +0530

    React App created in linux env

---
+++ linux-laptop-react/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello! I am Swapnaneel. Welcome to my blog!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/sw1pgh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit my Github Profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
